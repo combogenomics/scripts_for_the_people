3a9621b178d622b9578ad5266bd252071812beda
Gabriel Innocenti <gabrielinnocenti.g@gmail.com>
2023-03-09 10:13:13 +0100

    Add files via upload
    Add run file for muliGenomeLoader

---
+++ multigenomeLoader/runGammaproteobacteria.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+# Run multiGenomeLoader on all Gammaproteobacteria
+# Download assembly files
+wget https://ftp.ncbi.nlm.nih.gov/genomes/genbank/assembly_summary_genbank.txt
+wget https://ftp.ncbi.nlm.nih.gov/genomes/refseq/assembly_summary_refseq.txt
+
+# Unzip taxonomy names and tree files
+tar xz taxonomyNames.pkl.xz
+tar xz taxonomyParsedTree.pkl.xz
+
+# Run Loader
+python3 multiGenomeLoader.py Gammaproteobacteria --download
